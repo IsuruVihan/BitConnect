2622b97023182c47a1955629fb320a57ea43b412
Avishna Arambawela <111332549+Avishna-00@users.noreply.github.com>
2024-03-15 09:25:55 +0530

    chatbox UI issue

--- src/components/pages/ChatBox.jsx
+++ src/components/pages/ChatBox.jsx
@@ -12,30 +12,39 @@ const people = [
 const ChatBox = () => {
     return(
         <div>
-            <main className="lg:pl-72">
-                <div className="xl:pl-96">
-                    <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
-                        {/* Main area */}
+            <main className="">
+                <div className="">
+                    <div className="">
+                        {/* Main area started*/}
+                        <div className="border-2 border-fuchsia-800 h-1/5 min-w-full">
+                            dfgdfgdbbfbfbffgbhghfggbfg
+                        </div>
+                        <div className="border-2 border-fuchsia-800 h-3/5 min-w-full">
+                            fgfdnrswascfnhnjj
+                        </div>
+                        <div className="border-2 border-fuchsia-800 h-1/5 min-w-full">
+                            twefsdcscscawavnhmg
+                        </div>
+                        {/* Main area end*/}
 
-
-                        
                     </div>
                 </div>
             </main>
 
-            <aside className="fixed inset-y-0 hidden px-4 py-6 overflow-y-auto border-r border-gray-200 left-72 w-96 sm:px-6 lg:px-8 xl:block">
-                
+            <aside
+                className="fixed inset-y-0 overflow-y-auto border-r border-gray-200 left-72 w-96 xl:block">
+
                 {/* add search bar here */}
-                
+
                 <ul role="list" className="pt-10 divide-y divide-gray-200">
                     {people.map((person, id) => (
                         <li key={id} className="px-4 py-4 sm:px-0">
-                        
+
                             {/* My content */}
                             <div
                                 key={person.email}
                                 className="relative flex items-center px-2 py-5 space-x-3 bg-white border border-gray-300 rounded-lg shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
-                            >                                
+                            >
                                 <div className="flex-shrink-0">
                                     <img className="w-10 h-10 rounded-full" src={person.imageUrl} alt="" />
                                 </div>
