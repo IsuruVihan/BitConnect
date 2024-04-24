b7273f11137c33100c0adfc3f0835695ca9871c4
Avishna Arambawela <111332549+Avishna-00@users.noreply.github.com>
2024-04-25 05:25:25 +0530

    refinements on Attendance UI(2)

--- src/pages/Attendance.jsx
+++ src/pages/Attendance.jsx
@@ -25,14 +25,14 @@ const Attendance = () => {
         <div className="flex flex-col space-y-4 mb-4">
           <input
             type="text"
-            className="rounded-md border border-gray-300 px-3 py-2 w-96 text-center" // Added text-center class
+            className="rounded-md border border-gray-300 px-2 py-1 w-96 text-center" // Added text-center class
             placeholder="Date"
             value={checkInTime || ''}
             disabled
           />
           <input
             type="text"
-            className="rounded-md border border-gray-300 px-3 py-2 w-96 text-center" // Added text-center class
+            className="rounded-md border border-gray-300 px-2 py-1 w-96 text-center" // Added text-center class
             placeholder="Time"
             value={checkInTime || ''}
             disabled
@@ -42,13 +42,13 @@ const Attendance = () => {
         {/* Check in and Check out buttons */}
         <div className="flex space-x-4 mb-4">
           <button
-            className="block w-48 px-5 py-3 text-sm font-semibold text-center text-white bg-green-600 rounded-md shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+            className="block w-48 px-3 py-2 text-sm font-semibold text-center text-white bg-green-600 rounded-md shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
             onClick={handleCheckIn}
           >
             Check In
           </button>
           <button
-            className="block w-48 px-5 py-3 text-sm font-semibold text-center text-white bg-red-600 rounded-md shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+            className="block w-48 px-3 py-2 text-sm font-semibold text-center text-white bg-red-600 rounded-md shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
             onClick={handleCheckOut}
           >
             Check Out
@@ -58,7 +58,7 @@ const Attendance = () => {
         {/* Generate Report Button */}
         <div className="mb-4 ">
           <button
-            className="block w-96 px-5 py-3 text-sm font-semibold text-center text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="block w-96 px-3 py-2 text-sm font-semibold text-center text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             onClick={generateReport}
           >
             Generate Report
