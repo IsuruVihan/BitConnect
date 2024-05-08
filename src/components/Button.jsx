efd3fd452201d2c8c6de754d8e9a18a9332cb62b
Avishna Arambawela <111332549+Avishna-00@users.noreply.github.com>
2024-05-08 15:59:02 +0530

    CreateAttendanceReportModal with colored button issue

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,11 @@
-// under construction
-
 import React from "react";
 
-const Button = ({onClick, label, color, width}) => {
+const Button = ({onClick, label, color='indigo', fontColor= 'white', width}) => {
 	return (
 		<button
 			type="button"
-			className={`w-${width} px-3 py-2 mt-8 text-sm font-semibold text-white bg-${color}-600 rounded-md shadow
-			hover:bg-${color}-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+			className={`w-${width} px-3 py-2 mt-8 text-sm font-semibold text-${fontColor} bg-${color}-600 rounded-md shadow
+			hover:bg-${color}-500 focus-visible:outline focus-visible:outline-3 focus-visible:outline-offset-5
 			focus-visible:outline-${color}-600`}
 			onClick={onClick}
 		>
@@ -17,3 +15,5 @@ const Button = ({onClick, label, color, width}) => {
 }
 
 export default Button;
+
+
--- src/components/modals/CreateAttendanceReportModal.jsx
+++ src/components/modals/CreateAttendanceReportModal.jsx
@@ -1,8 +1,28 @@
-import React, {Fragment} from 'react';
-import {Dialog, Transition} from "@headlessui/react";
+import React, { useState, Fragment } from 'react';
+import { Dialog, Transition } from "@headlessui/react";
+import Button from "../Button";
 
 const CreateAttendanceReportModal = (props) => {
-  const {open, setOpen} = props;
+  const { open, setOpen } = props;
+
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+
+  const handleStartDateChange = (event) => {
+    setStartDate(event.target.value);
+  };
+
+  const handleEndDateChange = (event) => {
+    setEndDate(event.target.value);
+  };
+
+  const cancelGeneration = () => {
+
+  }
+
+  const proceedGeneration = () => {
+
+  }
 
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -37,7 +57,62 @@ const CreateAttendanceReportModal = (props) => {
 
                 {/*Modal body start*/}
                 <div>
-                  Heloo!
+
+                  <div className="flex items-center">
+                    {/*Start Date*/}
+                    <div className="relative">
+                      <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg
+                          className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                        >
+                          <path
+                            d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                        </svg>
+                      </div>
+                      <input
+                        name="start"
+                        type="text"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Select date start"
+                        value={startDate}
+                        onChange={handleStartDateChange}
+                      />
+                    </div>
+                    <span className="mx-4 text-gray-500">to</span>
+                    {/*End Date*/}
+                    <div className="relative">
+                      <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg
+                          className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                        >
+                          <path
+                            d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                        </svg>
+                      </div>
+                      <input
+                        name="end"
+                        type="text"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Select date end"
+                        value={endDate}
+                        onChange={handleEndDateChange}
+                      />
+                    </div>
+                  </div>
+                  {/*Buttons*/}
+                  <div className="flex space-x-20 items-center">
+                    <Button onClick={cancelGeneration} width={'48'} color={'white'} fontColor={'black'} label={'Cancel'}/>
+                    <Button onClick={proceedGeneration} width={'48'} color={'indigo'} label={'Generate'}/>
+                  </div>
+
                 </div>
                 {/*Modal body end*/}
 
