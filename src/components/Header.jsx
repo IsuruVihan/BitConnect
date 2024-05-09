60369cf5f4a81fa085cea5513dfebed999b0a2fb
Avishna Arambawela <111332549+Avishna-00@users.noreply.github.com>
2024-05-09 10:02:05 +0530

    my account - totally changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import { Fragment, useState } from 'react'
-import { Dialog, Menu, Transition } from '@headlessui/react'
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import {Fragment, useState} from 'react'
+import {Dialog, Menu, Transition} from '@headlessui/react'
+import {Route, Routes, useNavigate} from 'react-router-dom';
 
 import Attendance from '../pages/Attendance';
 import Leave from '../pages/Leave';
@@ -12,31 +12,21 @@ import Calendar from '../pages/Calendar';
 import EmpView from '../pages/EmpView';
 
 import {
-  CheckIcon,
-  EnvelopeIcon,
-  ArrowUpTrayIcon,
-  ChatBubbleLeftRightIcon,
-  ClipboardDocumentListIcon,
   AcademicCapIcon,
-  CalendarIcon,
-  IdentificationIcon,
-  XMarkIcon,
+  ArrowUpTrayIcon,
   Bars3Icon,
   BellIcon,
+  CalendarIcon,
+  ChatBubbleLeftRightIcon,
+  CheckIcon,
+  ClipboardDocumentListIcon,
+  EnvelopeIcon,
+  IdentificationIcon,
+  XMarkIcon,
+  UserIcon,
 } from '@heroicons/react/24/outline'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import {ChevronDownIcon} from '@heroicons/react/20/solid'
 import MyAccount from "../pages/MyAccount";
-
-const navigation = [
-  { name: 'Attendance', path: '/attendance', icon: CheckIcon, current: true },
-  { name: 'Leave', path: '/leave', icon: EnvelopeIcon, current: false },
-  { name: 'Common Wall', path: '/common-wall', icon:ArrowUpTrayIcon , current: false },
-  { name: 'ChatBox', path: '/chat-box', icon:ChatBubbleLeftRightIcon , current: false },
-  { name: 'Special Notices', path: '/special-notices', icon:ClipboardDocumentListIcon , current: false },
-  { name: 'KT Courses', path: '/kt-courses', icon:AcademicCapIcon , current: false },
-  { name: 'Calendar', path: '/calendar', icon:CalendarIcon, current: false },
-  { name: 'Employees View', path: '/employees-view', icon:IdentificationIcon, current: false },
-]
 
 // TODO : add onclicks for these
 const userNavigation = [
@@ -60,13 +50,21 @@ const Header = () => {
     { name: 'KT Courses', path: '/kt-courses', icon:AcademicCapIcon , current: false },
     { name: 'Calendar', path: '/calendar', icon:CalendarIcon, current: false },
     { name: 'Employees View', path: '/employees-view', icon:IdentificationIcon, current: false },
+    { name: 'My Account', path: '/my-account', icon:UserIcon, current: false },
   ])
-  
+
   const getCurrentNavigationName = () => {
-    const currentNavigationItem = navigation.find(item => item.current);
-    return currentNavigationItem ? currentNavigationItem.name : '';
+    const capitalizeEachWord = (str) => {
+      return str.replace(/\b\w/g, function(char) {
+        return char.toUpperCase();
+      });
+    }
+
+    const currentURL = window.location.href;
+    const label = currentURL.split('/').slice(-1)[0];
+    return capitalizeEachWord(label.replace(/-/g, ' '));
   };
-  
+
   const redirect = (path) => {
     navigate(path)
     const updatedNavigation = navigation.map(item => {
@@ -76,9 +74,9 @@ const Header = () => {
         return { ...item, current: false };
       }
     });
-    setNavigation(updatedNavigation);    
+    setNavigation(updatedNavigation);
   }
-  
+
   return (
     <>
       <div>
@@ -202,9 +200,13 @@ const Header = () => {
         </div>
 
         <div className="lg:pl-72">
-          <div
-              className="sticky top-0 z-40 flex items-center h-16 px-4 bg-white border-b border-gray-200 shadow-sm shrink-0 gap-x-4 sm:gap-x-6 sm:px-6 lg:px-8">
-            <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden" onClick={() => setSidebarOpen(true)}>
+          <div className="sticky top-0 z-40 flex items-center h-16 px-4 bg-white border-b border-gray-200 shadow-sm
+          shrink-0 gap-x-4 sm:gap-x-6 sm:px-6 lg:px-8">
+            <button
+              type="button"
+              className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+              onClick={() => setSidebarOpen(true)}
+            >
               <span className="sr-only">Open sidebar</span>
               <Bars3Icon className="w-6 h-6" aria-hidden="true"/>
             </button>
@@ -231,9 +233,9 @@ const Header = () => {
                   <Menu.Button className="-m-1.5 flex items-center p-1.5">
                     <span className="sr-only">Open user menu</span>
                     <img
-                        className="w-8 h-8 rounded-full bg-gray-50"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
+                      className="w-8 h-8 rounded-full bg-gray-50"
+                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      alt=""
                     />
                     <span className="hidden lg:flex lg:items-center">
                       <span className="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">
@@ -243,30 +245,32 @@ const Header = () => {
                     </span>
                   </Menu.Button>
                   <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items
-                        className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                      className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg
+                      ring-1 ring-gray-900/5 focus:outline-none"
+                    >
                       {userNavigation.map((item) => (
-                          <Menu.Item key={item.name}>
-                            {({active}) => (
-                                <a
-                                    href={item.href}
-                                    className={classNames(
-                                        active ? 'bg-gray-50' : '',
-                                        'block px-3 py-1 text-sm leading-6 text-gray-900'
-                                    )}
-                                >
-                                  {item.name}
-                                </a>
-                            )}
-                          </Menu.Item>
+                        <Menu.Item key={item.name}>
+                          {({active}) => (
+                            <a
+                              href={item.href}
+                              className={classNames(
+                                active ? 'bg-gray-50' : '',
+                                'block px-3 py-1 text-sm leading-6 text-gray-900'
+                              )}
+                            >
+                              {item.name}
+                            </a>
+                          )}
+                        </Menu.Item>
                       ))}
                     </Menu.Items>
                   </Transition>
--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -1,336 +1,84 @@
 import React from "react";
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import {EnvelopeIcon} from '@heroicons/react/20/solid'
+
+const profile = {
+	name: 'Ricardo Cooper',
+	imageUrl:
+		'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+	coverImageUrl:
+		'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+	fields: {
+		Phone: '(555) 123-4567',
+		Email: 'ricardocooper@example.com',
+		Title: 'Senior Front-End Developer',
+		Team: 'Product Development',
+		Birthday: 'June 8, 1990',
+	},
+}
+
+function classNames(...classes) {
+	return classes.filter(Boolean).join(' ')
+}
 
 const MyAccount = () => {
-	return(
-		<div>
-			{/*<form>*/}
-			{/*	<div className="space-y-12">*/}
-			{/*		<div className="border-b border-gray-900/10 pb-12">*/}
-			{/*			<div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">*/}
-			{/*				<div className="sm:col-span-4">*/}
-			{/*					<label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*						Username*/}
-			{/*					</label>*/}
-			{/*					<div className="mt-2">*/}
-			{/*						<div*/}
-			{/*							className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">*/}
-			{/*							<span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>*/}
-			{/*							<input*/}
-			{/*								type="text"*/}
-			{/*								name="username"*/}
-			{/*								id="username"*/}
-			{/*								autoComplete="username"*/}
-			{/*								className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"*/}
-			{/*								placeholder="janesmith"*/}
-			{/*							/>*/}
-			{/*						</div>*/}
-			{/*					</div>*/}
-			{/*				</div>*/}
-
-			{/*				<div className="col-span-full">*/}
-			{/*					<label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*						About*/}
-			{/*					</label>*/}
-			{/*					<div className="mt-2">*/}
-      {/*          <textarea*/}
-			{/*						id="about"*/}
-			{/*						name="about"*/}
-			{/*						rows={3}*/}
-			{/*						className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"*/}
-			{/*						defaultValue={''}*/}
-			{/*					/>*/}
-			{/*					</div>*/}
-			{/*					<p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>*/}
-			{/*				</div>*/}
-
-			{/*				<div className="col-span-full">*/}
-			{/*					<label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*						Photo*/}
-			{/*					</label>*/}
-			{/*					<div className="mt-2 flex items-center gap-x-3">*/}
-			{/*						<UserCircleIcon className="h-12 w-12 text-gray-300" aria-hidden="true"/>*/}
-			{/*						<button*/}
-			{/*							type="button"*/}
-			{/*							className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"*/}
-			{/*						>*/}
-			{/*							Change*/}
-			{/*						</button>*/}
-			{/*					</div>*/}
-			{/*				</div>*/}
-
-			{/*				<div className="col-span-full">*/}
-			{/*					<label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*						Cover photo*/}
-			{/*					</label>*/}
-			{/*					<div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">*/}
-			{/*						<div className="text-center">*/}
-			{/*							<PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true"/>*/}
-			{/*							<div className="mt-4 flex text-sm leading-6 text-gray-600">*/}
-			{/*								<label*/}
-			{/*									htmlFor="file-upload"*/}
-			{/*									className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"*/}
-			{/*								>*/}
-			{/*									<span>Upload a file</span>*/}
-			{/*									<input id="file-upload" name="file-upload" type="file" className="sr-only"/>*/}
-			{/*								</label>*/}
-			{/*								<p className="pl-1">or drag and drop</p>*/}
-			{/*							</div>*/}
-			{/*							<p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>*/}
-			{/*						</div>*/}
-			{/*					</div>*/}
-			{/*				</div>*/}
-			{/*			</div>*/}
-			{/*		</div>*/}
-
-			{/*		<div className="border-b border-gray-900/10 pb-12">*/}
-			{/*			<h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>*/}
-			{/*			<p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>*/}
-
-			{/*			<div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">*/}
-			{/*				<div className="sm:col-span-3">*/}
-			{/*					<label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*						First name*/}
-			{/*					</label>*/}
-			{/*					<div className="mt-2">*/}
-			{/*						<input*/}
-			{/*							type="text"*/}
-			{/*							name="first-name"*/}
-			{/*							id="first-name"*/}
-			{/*							autoComplete="given-name"*/}
-			{/*							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"*/}
-			{/*						/>*/}
-			{/*					</div>*/}
-			{/*				</div>*/}
-
-			{/*				<div className="sm:col-span-3">*/}
-			{/*					<label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*						Last name*/}
-			{/*					</label>*/}
-			{/*					<div className="mt-2">*/}
-			{/*						<input*/}
-			{/*							type="text"*/}
-			{/*							name="last-name"*/}
-			{/*							id="last-name"*/}
-			{/*							autoComplete="family-name"*/}
-			{/*							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"*/}
-			{/*						/>*/}
-			{/*					</div>*/}
-			{/*				</div>*/}
-
-			{/*				<div className="sm:col-span-4">*/}
-			{/*					<label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*						Email address*/}
-			{/*					</label>*/}
-			{/*					<div className="mt-2">*/}
-			{/*						<input*/}
-			{/*							id="email"*/}
-			{/*							name="email"*/}
-			{/*							type="email"*/}
-			{/*							autoComplete="email"*/}
-			{/*							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"*/}
-			{/*						/>*/}
-			{/*					</div>*/}
-			{/*				</div>*/}
-
-			{/*				<div className="sm:col-span-3">*/}
-			{/*					<label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*						Country*/}
-			{/*					</label>*/}
-			{/*					<div className="mt-2">*/}
-			{/*						<select*/}
-			{/*							id="country"*/}
-			{/*							name="country"*/}
-			{/*							autoComplete="country-name"*/}
-			{/*							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"*/}
-			{/*						>*/}
-			{/*							<option>United States</option>*/}
-			{/*							<option>Canada</option>*/}
-			{/*							<option>Mexico</option>*/}
-			{/*						</select>*/}
-			{/*					</div>*/}
-			{/*				</div>*/}
-
-			{/*				<div className="col-span-full">*/}
-			{/*					<label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*						Street address*/}
-			{/*					</label>*/}
-			{/*					<div className="mt-2">*/}
-			{/*						<input*/}
-			{/*							type="text"*/}
-			{/*							name="street-address"*/}
-			{/*							id="street-address"*/}
-			{/*							autoComplete="street-address"*/}
-			{/*							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"*/}
-			{/*						/>*/}
-			{/*					</div>*/}
-			{/*				</div>*/}
-
-			{/*				<div className="sm:col-span-2 sm:col-start-1">*/}
-			{/*					<label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*						City*/}
-			{/*					</label>*/}
-			{/*					<div className="mt-2">*/}
-			{/*						<input*/}
-			{/*							type="text"*/}
-			{/*							name="city"*/}
-			{/*							id="city"*/}
-			{/*							autoComplete="address-level2"*/}
-			{/*							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"*/}
-			{/*						/>*/}
-			{/*					</div>*/}
-			{/*				</div>*/}
-
-			{/*				<div className="sm:col-span-2">*/}
-			{/*					<label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*						State / Province*/}
-			{/*					</label>*/}
-			{/*					<div className="mt-2">*/}
-			{/*						<input*/}
-			{/*							type="text"*/}
-			{/*							name="region"*/}
-			{/*							id="region"*/}
-			{/*							autoComplete="address-level1"*/}
-			{/*							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"*/}
-			{/*						/>*/}
-			{/*					</div>*/}
-			{/*				</div>*/}
-
-			{/*				<div className="sm:col-span-2">*/}
-			{/*					<label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*						ZIP / Postal code*/}
-			{/*					</label>*/}
-			{/*					<div className="mt-2">*/}
-			{/*						<input*/}
-			{/*							type="text"*/}
-			{/*							name="postal-code"*/}
-			{/*							id="postal-code"*/}
-			{/*							autoComplete="postal-code"*/}
-			{/*							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"*/}
-			{/*						/>*/}
-			{/*					</div>*/}
-			{/*				</div>*/}
-			{/*			</div>*/}
-			{/*		</div>*/}
-
-			{/*		<div className="border-b border-gray-900/10 pb-12">*/}
-			{/*			<h2 className="text-base font-semibold leading-7 text-gray-900">Notifications</h2>*/}
-			{/*			<p className="mt-1 text-sm leading-6 text-gray-600">*/}
-			{/*				We'll always let you know about important changes, but you pick what else you want to hear about.*/}
-			{/*			</p>*/}
-
-			{/*			<div className="mt-10 space-y-10">*/}
-			{/*				<fieldset>*/}
-			{/*					<legend className="text-sm font-semibold leading-6 text-gray-900">By Email</legend>*/}
-			{/*					<div className="mt-6 space-y-6">*/}
-			{/*						<div className="relative flex gap-x-3">*/}
-			{/*							<div className="flex h-6 items-center">*/}
-			{/*								<input*/}
-			{/*									id="comments"*/}
-			{/*									name="comments"*/}
-			{/*									type="checkbox"*/}
-			{/*									className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"*/}
-			{/*								/>*/}
-			{/*							</div>*/}
-			{/*							<div className="text-sm leading-6">*/}
-			{/*								<label htmlFor="comments" className="font-medium text-gray-900">*/}
-			{/*									Comments*/}
-			{/*								</label>*/}
-			{/*								<p className="text-gray-500">Get notified when someones posts a comment on a posting.</p>*/}
-			{/*							</div>*/}
-			{/*						</div>*/}
-			{/*						<div className="relative flex gap-x-3">*/}
-			{/*							<div className="flex h-6 items-center">*/}
-			{/*								<input*/}
-			{/*									id="candidates"*/}
-			{/*									name="candidates"*/}
-			{/*									type="checkbox"*/}
-			{/*									className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"*/}
-			{/*								/>*/}
-			{/*							</div>*/}
-			{/*							<div className="text-sm leading-6">*/}
-			{/*								<label htmlFor="candidates" className="font-medium text-gray-900">*/}
-			{/*									Candidates*/}
-			{/*								</label>*/}
-			{/*								<p className="text-gray-500">Get notified when a candidate applies for a job.</p>*/}
-			{/*							</div>*/}
-			{/*						</div>*/}
-			{/*						<div className="relative flex gap-x-3">*/}
-			{/*							<div className="flex h-6 items-center">*/}
-			{/*								<input*/}
-			{/*									id="offers"*/}
-			{/*									name="offers"*/}
-			{/*									type="checkbox"*/}
-			{/*									className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"*/}
-			{/*								/>*/}
-			{/*							</div>*/}
-			{/*							<div className="text-sm leading-6">*/}
-			{/*								<label htmlFor="offers" className="font-medium text-gray-900">*/}
-			{/*									Offers*/}
-			{/*								</label>*/}
-			{/*								<p className="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>*/}
-			{/*							</div>*/}
-			{/*						</div>*/}
-			{/*					</div>*/}
-			{/*				</fieldset>*/}
-			{/*				<fieldset>*/}
-			{/*					<legend className="text-sm font-semibold leading-6 text-gray-900">Push Notifications</legend>*/}
-			{/*					<p className="mt-1 text-sm leading-6 text-gray-600">These are delivered via SMS to your mobile*/}
-			{/*						phone.</p>*/}
-			{/*					<div className="mt-6 space-y-6">*/}
-			{/*						<div className="flex items-center gap-x-3">*/}
-			{/*							<input*/}
-			{/*								id="push-everything"*/}
-			{/*								name="push-notifications"*/}
-			{/*								type="radio"*/}
-			{/*								className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"*/}
-			{/*							/>*/}
-			{/*							<label htmlFor="push-everything" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*								Everything*/}
-			{/*							</label>*/}
-			{/*						</div>*/}
-			{/*						<div className="flex items-center gap-x-3">*/}
-			{/*							<input*/}
-			{/*								id="push-email"*/}
-			{/*								name="push-notifications"*/}
-			{/*								type="radio"*/}
-			{/*								className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"*/}
-			{/*							/>*/}
-			{/*							<label htmlFor="push-email" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*								Same as email*/}
-			{/*							</label>*/}
-			{/*						</div>*/}
-			{/*						<div className="flex items-center gap-x-3">*/}
-			{/*							<input*/}
-			{/*								id="push-nothing"*/}
-			{/*								name="push-notifications"*/}
-			{/*								type="radio"*/}
-			{/*								className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"*/}
-			{/*							/>*/}
-			{/*							<label htmlFor="push-nothing" className="block text-sm font-medium leading-6 text-gray-900">*/}
-			{/*								No push notifications*/}
-			{/*							</label>*/}
-			{/*						</div>*/}
-			{/*					</div>*/}
-			{/*				</fieldset>*/}
-			{/*			</div>*/}
-			{/*		</div>*/}
-			{/*	</div>*/}
-
-			{/*	<div className="mt-6 flex items-center justify-end gap-x-6">*/}
-			{/*		<button type="button" className="text-sm font-semibold leading-6 text-gray-900">*/}
-			{/*			Cancel*/}
-			{/*		</button>*/}
-			{/*		<button*/}
-			{/*			type="submit"*/}
-			{/*			className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"*/}
-			{/*		>*/}
-			{/*			Save*/}
-			{/*		</button>*/}
-			{/*	</div>*/}
-			{/*</form>*/}
+	return (
+		<div className="flex h-full">
+			<div className="flex min-w-0 flex-1 flex-col overflow-hidden">
+				<div className="relative z-0 flex flex-1 overflow-hidden">
+					<main className="relative z-0 flex-1 overflow-y-auto focus:outline-none xl:order-last">
+						<article>
+							<div>
+								<div>
+									<img className="h-32 w-full object-cover lg:h-48" src={profile.coverImageUrl} alt=""/>
+								</div>
+								<div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+									<div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+										<div className="flex">
+											<img
+												className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
+												src={profile.imageUrl}
+												alt=""
+											/>
+										</div>
+										<div
+											className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+											<div className="mt-6 min-w-0 flex-1 sm:hidden 2xl:block">
+												<h1 className="truncate text-2xl font-bold text-gray-900">{profile.name}</h1>
+											</div>
+											<div
+												className="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-x-4 sm:space-y-0">
+												<button
+													type="button"
+													className="inline-flex justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm
+													font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+												>
+													<EnvelopeIcon className="-ml-0.5 h-5 w-5 text-gray-400" aria-hidden="true"/>
+													Message
+												</button>
+											</div>
+										</div>
+									</div>
+									<div className="mt-6 hidden min-w-0 flex-1 sm:block 2xl:hidden">
+										<h1 className="truncate text-2xl font-bold text-gray-900">{profile.name}</h1>
+									</div>
+								</div>
+							</div>
+							{/* Description list */}
+							<div className="mx-auto mt-6 max-w-5xl px-4 sm:px-6 lg:px-8">
+								<dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+									{Object.keys(profile.fields).map((field) => (
+										<div key={field} className="sm:col-span-1">
+											<dt className="text-sm font-medium text-gray-500">{field}</dt>
+											<dd className="mt-1 text-sm text-gray-900">{profile.fields[field]}</dd>
+										</div>
+									))}
+								</dl>
+							</div>
+						</article>
+					</main>
+				</div>
+			</div>
 		</div>
-	)
-
+	);
 }
 export default MyAccount;
