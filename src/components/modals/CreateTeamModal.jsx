7b934d3b4cd7427b7c0654c1aaa084ba0b8a6e4c
Avishna Arambawela <111332549+Avishna-00@users.noreply.github.com>
2024-06-05 23:39:34 +0530

    create team front end done

--- src/components/modals/CreateTeamModal.jsx
+++ src/components/modals/CreateTeamModal.jsx
@@ -2,17 +2,10 @@ import React, {Fragment} from 'react';
 import {Dialog, Listbox, Transition} from "@headlessui/react";
 import {SecondaryButton, SuccessButton} from "../Button";
 
-const CreateEmpAccModal = (props) => {
-	const {open, setOpen, firstName, setFirstName, lastName, setLastName, empEmail, setEmpEmail, empRole, setEmpRole, empTeam, setEmpTeam, joinedDate, setJoinedDate, createAccount} = props;
+const CreateTeamModal = (props) => {
+	const {open, setOpen, teamName, setTeamName, clientName, setClientName} = props;
 
-	const emptyFirstName = firstName.trim() === "";
-	const emptyLastName = lastName.trim() === "";
-	const emptyEmail = empEmail.trim() === "";
-	// need to learn how Role and Team applicable here
-
-
-
-	return(
+	return (
 		<Transition.Root show={open} as={Fragment}>
 			<Dialog as="div" className="relative z-10" onClose={() => {
 			}}>
@@ -46,124 +39,45 @@ const CreateEmpAccModal = (props) => {
 
 								{/*Modal body start*/}
 								<div className=" sm:items-center mb-4 border-red-500">
-										<div className="sm:flex-auto">
-											<h1 className="text-base font-semibold leading-6 text-gray-900">Create an User Account</h1>
-											<p className="mt-1 mb-5 text-sm text-gray-700">
-												Enter relevant details of new employee.
-											</p>
-										</div>
-
-									<div className="flex sm:flex-row flex-col gap-4 items-center justify-between mt-3">
-										<div className="w-full">
-											<label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-												First Name
-											</label>
-											<div className="mt-1">
-												<input
-													type="text"
-													name="firstname"
-													id="fname"
-													className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset
-													ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600
-													sm:text-sm sm:leading-6"
-													value={firstName}
-												/>
-											</div>
-										</div>
-										<div className="w-full">
-											<label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
-												Last Name
-											</label>
-											<div className="mt-1">
-												<input
-													type="text"
-													name="lastname"
-													id="lname"
-													className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset
-													ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600
-													sm:text-sm sm:leading-6"
-													value={lastName}
-												/>
-											</div>
-										</div>
-									</div>
-
-									<div className="w-full mt-3">
-										<label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-											Email
-										</label>
-										<div className="mt-1">
-											<input
-												type="email"
-												name="email"
-												id="email"
-												className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset
-                        ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600
-                        sm:text-sm sm:leading-6"
-												value={empEmail}
-											/>
-										</div>
+									<div className="sm:flex-auto">
+										<h1 className="text-base font-semibold leading-6 text-gray-900">Create a Project Team</h1>
+										<p className="mt-1 mb-5 text-sm text-gray-700">
+											Enter relevant details of new team.
+										</p>
 									</div>
 
 									<div className="flex sm:flex-row flex-col gap-4 items-center justify-between mt-3">
 										<div className="w-full">
-											<label htmlFor="team" className="block text-sm font-medium leading-6 text-gray-900">
-												Team
+											<label htmlFor="team-name" className="block text-sm font-medium leading-6 text-gray-900">
+												Team Name
 											</label>
 											<div className="mt-1">
-												<select
-													name="team"
-													id="team"
+												<input
+													type="text"
+													name="teamname"
+													id="teamname"
 													className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset
-      											ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600
-      											sm:text-sm sm:leading-6"
-													value={empTeam}
-													onChange={(e) => setEmpTeam(e.target.value)}
-												>
-													<option value="Team A">Team A</option>
-													<option value="Team B">Team B</option>
-													<option value="Team C">Team C</option>
-												</select>
+													ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600
+													sm:text-sm sm:leading-6"
+													value={teamName}
+												/>
 											</div>
 										</div>
 										<div className="w-full">
-											<label htmlFor="role" className="block text-sm font-medium leading-6 text-gray-900">
-												Role
+											<label htmlFor="client-name" className="block text-sm font-medium leading-6 text-gray-900">
+												Client Name
 											</label>
 											<div className="mt-1">
-												<select
-													name="role"
-													id="role"
+												<input
+													type="text"
+													name="clientname"
+													id="clientname"
 													className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset
-															ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600
-															sm:text-sm sm:leading-6"
-													value={empRole}
-													onChange={(e) => setEmpRole(e.target.value)}
-												>
-													<option value="BA">BA</option>
-													<option value="SE">SE</option>
-													<option value="QA">QA</option>
-													<option value="Team Lead">Team Lead</option>
-												</select>
+													ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600
+													sm:text-sm sm:leading-6"
+													value={clientName}
+												/>
 											</div>
-										</div>
-									</div>
-
-									<div className="w-full mt-3">
-										<label htmlFor="joined-date" className="block text-sm font-medium leading-6 text-gray-900">
-											Joined On
-										</label>
-										<div className="mt-1">
-											<input
-												type="date"
-												name="joinedDate"
-												id="joinedDate"
-												autoComplete="to"
-												className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset
-											ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600
-											sm:text-sm sm:leading-6"
-												value={joinedDate}
-											/>
 										</div>
 									</div>
 
@@ -240,8 +154,6 @@ const CreateEmpAccModal = (props) => {
 									{/*	{displayErrors()}*/}
 
 
-
-
 								</div>
 								<div className="flex flex-row items-center justify-between px-4 mt-6">
 									<SecondaryButton
@@ -264,4 +176,4 @@ const CreateEmpAccModal = (props) => {
 	);
 
 };
-export default CreateEmpAccModal;
+export default CreateTeamModal ;
